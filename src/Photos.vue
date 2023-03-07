36318dad0b3794a33cb7858f7da76fa9f9727040
rafnuss <rafnuss@gmail.com>
2023-03-06 20:56:43 -0500

    Add fetch to server

--- src/Photos.vue
+++ src/Photos.vue
@@ -1,8 +1,8 @@
 <template>
   <b-container fluid class="p-4 bg-dark" style="overflow-y: hidden">
     <b-row class="flex-row flex-nowrap">
-      <b-col v-for="s in slides" :key="s.image" md="2">
-        <b-img-lazy @click.native="openModal(s.image)" thumbnail fluid :src="s.image" />
+      <b-col v-for="p in photos" :key="p.uid" md="2">
+        <b-img-lazy @click.native="openModal(p.url)" thumbnail fluid :src="p.url + '=w1920-h1080'" />
       </b-col>
     </b-row>
     <b-modal ref="modal" hide-footer centered hide-header size="xl">
@@ -16,56 +16,7 @@ export default {
   data() {
     return {
       modal_link: "",
-      slides: [
-        {
-          image: "https://placekitten.com/800/400",
-          title: "Slide 1",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        },
-        {
-          image: "https://placekitten.com/800/401",
-          title: "Slide 2",
-          description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-          image: "https://placekitten.com/800/402",
-          title: "Slide 3",
-          description:
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        },
-        {
-          image: "https://placekitten.com/800/403",
-          title: "Sligfjude 1",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        },
-        {
-          image: "https://placekitten.com/800/404",
-          title: "Slidfgde 2",
-          description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-          image: "https://placekitten.com/800/405",
-          title: "Slide fgj3",
-          description:
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        },
-        {
-          image: "https://placekitten.com/800/406",
-          title: "Slidfgde 1",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        },
-        {
-          image: "https://placekitten.com/800/407",
-          title: "Slitrzde 2",
-          description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        },
-        {
-          image: "https://placekitten.com/800/408",
-          title: "Slidewer 3",
-          description:
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        },
-      ],
+      photos: [],
     };
   },
   methods: {
@@ -73,8 +24,36 @@ export default {
       this.modal_link = link;
       this.$refs["modal"].show();
     },
+    async loadPhotos() {
+      const response = await fetch("http://farwest-photos.raphaelnussbaumer.com/");
+      if (!response.ok) {
+        throw new Error("Network response was not OK");
+      }
+      const json = await response.json();
+      if (json != null) {
+        console.log(json);
+        this.photos = json;
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
-  mounted() {},
+  created() {
+    const maxTries = 3;
+    let tries = 0;
+    while (tries < maxTries) {
+      let res = this.loadPhotos();
+      //console.log(JSON.stringify(re, null, 2))
+      if (res) {
+        console.log(`Success after: ${tries}.`);
+        tries = 1000;
+      } else {
+        tries++;
+        console.log(`Error: ${tries}. Retrying...`);
+      }
+    }
+  },
 };
 </script>
 
