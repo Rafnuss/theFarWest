a51bcb0056164e9dba2fec7268c636c14475b684
Améline Nussbaumer <ameline.nussbaumer@gmail.com>
2023-03-18 13:09:19 -0400

    Create pres.js

---
+++ src/assets/pres.js
@@ -0,0 +1,33 @@
+
+const pres = [
+{
+  "region": "midwest",
+  "text": `
+  <p>test</p>
+  `
+},
+{
+  "region": "colorado",
+  "text":``
+},
+{
+  "region": "texas",
+  "text":``
+},
+{
+  "region": "arizona",
+  "text":``
+},
+{
+  "region": "california",
+  "text":``
+},
+{
+  "region": "mountainwest",
+  "text":``
+},
+{
+  "region": "cascadia",
+  "text":``
+},
+];
