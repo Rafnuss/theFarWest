ec5482fae8eec2ec11e7d3a417c9425c2f737913
rafnuss <rafnuss@gmail.com>
2023-03-25 22:38:56 -0400

    tweaking defis

--- src/Defis.vue
+++ src/Defis.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-modal id="modal-defis" scrollable title="Les défis de Mady" size="l" hide-footer centered>
+    <b-modal id="modal-defis" scrollable title="Les défis de Mady" size="lg" hide-footer centered>
       <p>
         Suivez ma liste de coches avec ces 10 premiers défis reçus - quand je les aurai tous cochés, à moi de vous en
         donner un !
@@ -10,13 +10,12 @@
         small
         responsive
         :fields="[
-          { key: 'index', label: '', class: 'text-center' },
           { key: 'done', label: '', class: 'text-center' },
           { key: 'name', label: 'Défi' },
+          { key: 'diff', label: 'Difficulté', class: 'text-center' },
         ]"
         :items="defis"
       >
-        <template #cell(index)="data"> {{ data.index + 1 }}. </template>
         <template #cell(done)="d">
           <template v-if="d.done">
             <b-icon icon="check-square" variant="success" />
@@ -25,18 +24,14 @@
             <b-icon icon="square" />
           </template>
         </template>
+        <template #cell(name)="d"> {{ d.index + 1 }}. {{ d.value }}</template>
+        <template #cell(diff)="d">
+          <b-icon icon="binoculars-fill" :variant="d.value == 1 ? 'success' : d.value == 2 ? 'warning' : 'danger'" />
+        </template>
       </b-table>
-      <h1>Les défis en Image</h1>
-      <b-carousel
-        v-model="slide"
-        controls
-        indicators
-        background="#ababab"
-        style="text-shadow: 1px 1px 2px #333"
-        :interval="0"
-      >
+      <b-carousel controls indicators background="#ababab" style="text-shadow: 1px 1px 2px #333" :interval="0">
         <b-carousel-slide
-          v-for="(d, i) in defis"
+          v-for="(d, i) in defis.filter((d) => d.done)"
           :key="d.name"
           :caption="'Defi ' + (i + 1)"
           :text="d.name"
@@ -56,40 +51,22 @@ export default {
           name: "Name a bird",
           done: false,
           src: "https://drive.google.com/uc?export=view&id=1oHkXd_poVr_WhujQnrqfPXlJje29RPtn",
+          diff: 1,
         },
         {
           name: "Run two miles",
           done: false,
           src: "https://drive.google.com/uc?export=view&id=1eOBqVU_2FhTNpIuLqSDhar8HEVcm_U0A",
+          diff: 2,
         },
         {
           name: "draw apicture",
           done: true,
           src: "https://drive.google.com/uc?export=view&id=1Q1W75BogCuFzGd5WETvTuzG-ZwfN1nau",
+          diff: 3,
         },
       ],
     };
-  },
-  methods: {
-    onSubmit(event) {
-      this.update = true;
-      if (event) {
-        event.preventDefault();
-      }
-      let t = species_list;
-      if (this.exclureLifer) {
-        t = t.filter((sp) => sp.world_lifer);
-      }
-      if (this.onlyTarget) {
-        t = t.filter((sp) => sp.target);
-      }
-      t = t.filter((sp) => this.tickSelected.includes(sp.tick));
-      t = t.filter((sp) => sp.common_name.toLowerCase().includes(this.filterCommonName.toLowerCase()));
-      this.species_list_show = t;
-      setTimeout(() => {
-        this.update = false;
-      }, 1500);
-    },
   },
 };
 </script>
